9a94d8fd7d4ed7e07524a5e2142c56d754a11d86
Jonathan Risinger <Jonathan.Risinger@rutgers.edu>
2015-08-19 12:38:33 -0400

    Trying to change precompile setting

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module RCubed
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w[ active_admin.css active_admin.js ]
 
    # config.generators do |g|
    #   g.factory_girl false
