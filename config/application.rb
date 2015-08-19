9780cd7b8c65f2b4f23c8ec8a3d986e4f5a44564
Jonathan Risinger <Jonathan.Risinger@rutgers.edu>
2015-08-19 12:35:52 -0400

    Removed some excess assets

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,6 @@ module RCubed
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
-    config.assets.compile = true
 
    # config.generators do |g|
    #   g.factory_girl false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,5 @@ RCubed::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.assets.prefix = "/dev-assets"
 end
