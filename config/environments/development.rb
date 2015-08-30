3043739b50d94bec3e81ec96cc6ff7f1613df9df
Sakib <sakib.jalal@gmail.com>
2015-08-30 19:43:35 -0400

    MAILER WORKS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,11 @@ RCubed::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Trying this out
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,15 +37,15 @@ RCubed::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '45.55.59.212' }
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["yourGmail@gmail.com"],
-    password: ENV["yourPassword"]
+    :address    => "smtp.mandrillapp.com",
+    :port       => 25,
+    :user_name  => ENV["MANDRILL_USERNAME"],
+    :password   => ENV["MANDRILL_PASSWORD"]
   }
+
 end
